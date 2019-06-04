6b44f29a5c61148dbe8364ac222948bc0d32a8c8
dlwong <pingki@sbcglobal.net>
2019-06-03 21:58:07 -0700

    work in progress

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -1,13 +1,16 @@
 var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
+  // this = Object.create(makeBlinkyDancer.prototype);
   // var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
   makeDancer.call(this, top, left, timeBetweenSteps);
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
   // this.hello = 'hello';
-  this.oldStep = makeDancer.prototype.step;
+  // console.log(makeDancer)
+  this.oldStep = this.step();
+  // this.oldStep = this.step;
   
-  // return blinkyDancer;
+  // return this;
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
@@ -15,34 +18,16 @@ makeBlinkyDancer.prototype.constructor = makeBlinkyDancer;
 
 makeBlinkyDancer.prototype.step = function() {
   // call the old version of step at the beginning of any call to this new version of step
+  // var counter = 0;
+  // counter++;
+  // console.log(counter)
   this.oldStep();
-  // makeBlinkyDancer.step();
-  // console.log(this)
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
   this.$node.toggle();
+  console.log('sdf')
 };
 
 
 
-
-// var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
-//   var blinkyDancer = makeDancer(top, left, timeBetweenSteps);
-
-//   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
-//   // so we must keep a copy of the old version of this function
-
-//   var oldStep = blinkyDancer.step;
-
-//   blinkyDancer.step = function() {
-//     // call the old version of step at the beginning of any call to this new version of step
-//     oldStep();
-//     // toggle() is a jQuery method to show/hide the <span> tag.
-//     // See http://api.jquery.com/category/effects/ for this and
-//     // other effects you can use on a jQuery-wrapped html tag.
-//     blinkyDancer.$node.toggle();
-//   };
-
-//   return blinkyDancer;
-// };
